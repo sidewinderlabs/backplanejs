feca2d8e2232bf5790e4a64ff2386ecc29a2772f
Mark Birbeck <mark.birbeck@webBackplane.com>
2010-01-25 11:21:48 +0000

    Added support for unit-test driving from Ant.
    Fixes issue 35.
    Fixes issue 36.

--- tools/tests/unit-test-runner.js
+++ tools/tests/unit-test-runner.js
@@ -1,36 +1,7 @@
-(function () {
-	var loader = new YAHOO.util.YUILoader({
-		base: "http://ajax.googleapis.com/ajax/libs/yui/2.8.0r4/build/",
-		require: [ "logger", "yuitest"],
-		loadOptional: false,
-		combine: false,
-		filter: "MIN",
-		allowRollup: true,
-		onSuccess: function () {
-			var loader = new YAHOO.util.YUILoader({
-				base: "",
-				onSuccess: function () {
-					var logger = new YAHOO.tool.TestLogger();
+YAHOO.util.Event.onDOMReady(
+	function() {
+		var logger = new YAHOO.tool.TestLogger();
 
-					YAHOO.tool.TestRunner.run();
-				}
-			});
-			loader.addModule({
-				name: "tests",
-				type: "js",
-				path: "unit-tests.js"
-			});
-			loader.require("tests");
-			loader.insert();
-		}
-	});
-
-	loader.addModule({
-		name: "logger-css", type: "css", path: "logger/assets/logger.css"
-	});
-	loader.addModule({
-		name: "test-logger-css", type: "css", path: "yuitest/assets/testlogger.css"
-	});
-	loader.require("logger-css", "test-logger-css");
-	loader.insert();
-})();
+		YAHOO.tool.TestRunner.run();
+	}
+);
