14eec3aae87e2270de78e5a2563b784a2c97ca2a
Mark Birbeck <mark.birbeck@webBackplane.com>
2010-05-02 12:06:15 +0100

    Added missing semi-colons to unit-test.

--- third-party/uxf/unit-tests/ut-instance-standalone.js
+++ third-party/uxf/unit-tests/ut-instance-standalone.js
@@ -7,7 +7,7 @@ var suiteInstanceStandalone = new YAHOO.tool.TestSuite({
 var caseInstanceStandalone = new YAHOO.tool.TestCase({
 	name		:	"Test instance in standalone mode",
   setUp   : function() {
-    var testDIV = document.createElement("div")
+    var testDIV = document.createElement("div");
     this.testInstance = new Instance(testDIV);
   },
   
@@ -20,7 +20,7 @@ var caseInstanceStandalone = new YAHOO.tool.TestCase({
 	function() {
     var Assert = YAHOO.util.Assert;
     var someDOM = xmlParse("<x>hello</x>");
-    this.testInstance.replaceDocument(someDOM)
+    this.testInstance.replaceDocument(someDOM);
     Assert.areEqual(xmlText(this.testInstance.getDocument()),xmlText(someDOM));
     
 	},
@@ -29,18 +29,18 @@ var caseInstanceStandalone = new YAHOO.tool.TestCase({
 	function() {
     var Assert = YAHOO.util.Assert;
     var someDOM = xmlParse("<x>hello</x>");
-    this.testInstance.replaceDocument(someDOM)
+    this.testInstance.replaceDocument(someDOM);
     var someOtherDOM = xmlParse("<y>bonjour</y>");
-    this.testInstance.replaceDocument(someOtherDOM)
+    this.testInstance.replaceDocument(someOtherDOM);
     Assert.areEqual(xmlText(this.testInstance.getDocument()),xmlText(someOtherDOM));
 	},
 	testResetDOM:
 	function() {
     var Assert = YAHOO.util.Assert;
     var someDOM = xmlParse("<x>hello</x>");
-    this.testInstance.replaceDocument(someDOM)
+    this.testInstance.replaceDocument(someDOM);
     var someOtherDOM = xmlParse("<y>bonjour</y>");
-    this.testInstance.replaceDocument(someOtherDOM)
+    this.testInstance.replaceDocument(someOtherDOM);
     this.testInstance.reset();
     Assert.areEqual(xmlText(this.testInstance.getDocument()),xmlText(someDOM));
 	}
