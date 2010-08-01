47e93d6184913700054bb45dd428b4f677046d0d
mark.birbeck@webBackplane.com <mark.birbeck@webBackplane.com>
2010-08-01 21:44:29 +0100

    Added unit-tests for UX.addClassName(), UX.removeClassName() and UX.replaceClassName().

--- third-party/uxf/unit-tests/ut-ux-utils.js
+++ third-party/uxf/unit-tests/ut-ux-utils.js
@@ -54,5 +54,49 @@
  		})//new TestCase
 	); //suite.add( ... )
 
+	// Add test for class manipulation functions.
+	//
+	suite.add(
+		new YAHOO.tool.TestCase({
+			name : "Test UXUtils class functions",
+
+			setUp : function() {
+				this.element = document.createElement("div");
+				return;
+			}, // setUp()
+
+			tearDown : function() {
+				this.element = null;
+				return;
+			}, // tearDown()
+
+			"test: UX.addClassName()": function() {
+				Assert.isFalse(UX.hasClassName(this.element, "foo"), "Class 'foo' should not be present at start.");
+				UX.addClassName(this.element, "foo");
+				Assert.isTrue(UX.hasClassName(this.element, "foo"), "Class 'foo' has not been added.");
+				return;
+			},
+
+			"test: UX.removeClassName()": function() {
+				Assert.isFalse(UX.hasClassName(this.element, "foo"), "Class 'foo' should not be present at start.");
+				UX.addClassName(this.element, "foo");
+				Assert.isTrue(UX.hasClassName(this.element, "foo"), "Class 'foo' has not been added.");
+				UX.removeClassName(this.element, "foo");
+				Assert.isFalse(UX.hasClassName(this.element, "foo"), "Class 'foo' has not been removed.");
+				return;
+			},
+
+			"test: UX.replaceClassName()": function() {
+				Assert.isFalse(UX.hasClassName(this.element, "foo"), "Class 'foo' should not be present at start.");
+				UX.addClassName(this.element, "foo");
+				Assert.isTrue(UX.hasClassName(this.element, "foo"), "Class 'foo' has not been added.");
+				UX.replaceClassName(this.element, "foo", "bar");
+				Assert.isFalse(UX.hasClassName(this.element, "foo"), "Class 'foo' has not been removed.");
+				Assert.isTrue(UX.hasClassName(this.element, "bar"), "Class 'bar' has not been added.");
+				return;
+			}
+ 		})//new TestCase
+	); //suite.add( ... )
+
 	YAHOO.tool.TestRunner.add(suite);
 }());
